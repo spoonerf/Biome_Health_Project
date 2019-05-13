79f731d32ed23ffc5c9d5a406acc7a18e22adf20
spoonerf <spoonerfiona@gmail.com>
2019-05-13 17:02:20 +0100

    removing a line

--- filestructure_change.R
+++ filestructure_change.R
@@ -12,7 +12,6 @@ data_type<-"CT"
 year<-"2018"
 
 
-
 exif_out$new_file_structure<-paste(base_path,"/",exif_out$site,"/",exif_out$site_id,"/",data_type,"/" ,year,"/",year,"_", site_id,"_",exif_out$new_img_num,".JPG", sep= "")
 
 exif_out$new_dir_structure<-paste(base_path, exif_out$site,exif_out$site_id,data_type, year, sep= "/")
